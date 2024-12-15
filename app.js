2f948e6ecd06e3d57b55049363376ec2bdc669d1
Carlos Daniel S. R. <167386779+Kaitoshizin@users.noreply.github.com>
2024-12-15 18:11:01 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -6,25 +6,10 @@ let tentativas = 1;
 
 telaInicial();
 
-//lista = array
-
-//alguns comentários foram removidos, você ainda pode vê-los na versão anterior.
-
 function telaMensagem(tag, mensagem){
     let campo = document.querySelector(tag);
     campo.innerHTML = mensagem;
 }
-
-// acima vemos uma função com parâmetros, que são funções com detalhes especificados no seu titulo.
-
-// e nela fizemos algo para reduzir o código
-// criamos um padrão de comportamento.
-// antes teriamos de fazer da primeira forma
-// 2 linhas de código pra algo simples toda vez que fossemos usar uma tag diferente
-// com essa padronização, poderemos escrever apenas uma da seguinte maneira:
-// telaMensagem('h1', 'batatas não são super heróis');
-// isso faz o trabalho de pegar a tag que era o código: let titulo = document.querySelector('h1');
-// e ainda nos permite escrever a mensagem na mesma linha, onde usavamos o cíodigo: titulo.innerHTML = 'Jogo do Número Secreto';
 
 function telaInicial() {
 telaMensagem('h1','Jogo do Número Secreto');
@@ -55,16 +40,6 @@ function verificarChute() {
 }
 }
 
-// está é uma função simples, que já está escrito no html, aqui no js, nós demos a ela um trabalho - uma FUNÇÃO
-// getElementeById é porque nós temos dois botões, mas queremos um especifico.
-
-// function gerarNumero(){
-//     return parseInt(Math.random() * numerador + 1);
-// }
-// essa é uma função com retorno, ela retorna apenas o que pedimos a ela.
-
-// a função acima será substituída, por isso virou um comentário, vamos a nova versão com mais funcionalidades para o projeto:
-
 function gerarNumero() {
     let numeroEscolhido = parseInt(Math.random() * 10 + 1);
     let numerosNaLista = numeroJaSorteado.length;
@@ -81,11 +56,6 @@ function gerarNumero() {
         return numeroEscolhido;
     }
 }
-
-//Basicamente, isso faz com que não venhamos a ter números repetidos em partidas seguidas
-// a lista começa vazia, mas usamos um comando para por todo o numero sorteado dentro dela
-// e quando sorteamos algo q já está lá, ele busca gerar outro número no lugar
-// para não haver erros, é necessário um comando para limpar a lista quando estiver cheia
 
 palavraTentativas = tentativas > 1 ? 'tentativas' : 'tentativa'; 
 
